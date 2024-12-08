7716de6a77155e79a85d53c2172feea45ed5cbf9
harsh170403 <harshdhattarwal17@gmail.com>
2024-12-08 15:23:36 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
+  // ... other config options ...
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  }
 })
+
