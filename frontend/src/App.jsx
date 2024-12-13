51ca045b3e19da5af4ec94582c22a5dce129206c
harsh170403 <harshdhattarwal17@gmail.com>
2024-12-13 08:51:52 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const BASE_URL = 'http://13.201.76.245:5000';
+const BASE_URL = 'http://13.234.71.144:5000';
 
 function App() {
     const [todos, setTodos] = useState([]);
