30ac7ef01f935660bb81355526a2d033c09cc9ee
harsh170403 <harshdhattarwal17@gmail.com>
2024-12-17 04:22:08 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000', 
+        target: 'http://65.2.74.211:5000', 
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
